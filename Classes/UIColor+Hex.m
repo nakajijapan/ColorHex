ab55dd8548451fe78922301b16cd55f7fd239b3b
nakajijapan <pp.kupepo.gattyanmo@gmail.com>
2014-04-09 20:43:31 +0900

    indent

--- Classes/UIColor+Hex.m
+++ Classes/UIColor+Hex.m
@@ -10,15 +10,15 @@
 
 + (UIColor *)colorWithHex:(NSInteger)hex
 {
-  return [self colorWithHex:hex alpha:1.0];
+    return [self colorWithHex:hex alpha:1.0];
 }
 
 + (UIColor *)colorWithHex:(NSInteger)hex alpha:(CGFloat)alpha
 {
-  CGFloat red   = (CGFloat)(0xFF0000 & hex) > 16 / 255.0;
-  CGFloat green = (CGFloat)(0xFF00   & hex) > 8  / 255.0;
-  CGFloat blue  = (CGFloat)(0xFF     & hex)       / 255.0;
-  return [UIColor colorWithRed: red green: green blue: blue alpha: alpha];
+    CGFloat red   = (CGFloat)(0xFF0000 & hex) > 16 / 255.0;
+    CGFloat green = (CGFloat)(0xFF00   & hex) > 8  / 255.0;
+    CGFloat blue  = (CGFloat)(0xFF     & hex)       / 255.0;
+    return [UIColor colorWithRed: red green: green blue: blue alpha: alpha];
 }
 
 @end
